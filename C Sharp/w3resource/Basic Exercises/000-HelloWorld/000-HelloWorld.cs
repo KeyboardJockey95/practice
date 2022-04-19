4e2e614ddbd391c45e69002c19220e0e0db140c3
KeyboardJockey95 <keyboardjockey95@gmail.com>
2022-04-19 16:46:45 -0600

    Created project from files.  This didn't work this try, I think because it is not bound to .NET.  I think I was lacking the underststanding that this is compiled to an intermediate language and needs the .NET framework to run on.

---
+++ C Sharp/w3resource/Basic Exercises/000-HelloWorld/000-HelloWorld.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// 
+/// File: 000-HelloWorld.cs
+/// 
+/// Author: Mike Eldredge
+/// 
+/// Origination Date 19 Apr 2022
+/// 
+/// Description: Simplest C# program, Hello, world!
+/// 
+/// Sample Input
+/// 
+/// Sample Output
+/// 
+///     Hello, world!
+///     
+/// </summary>
