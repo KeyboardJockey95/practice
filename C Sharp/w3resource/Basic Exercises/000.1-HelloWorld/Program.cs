1fe2cbaa98cace29404a49d1ed865de9e89c8a7a
KeyboardJockey95 <keyboardjockey95@gmail.com>
2022-04-19 17:14:20 -0600

    Default new console app project skeleton

---
+++ C Sharp/w3resource/Basic Exercises/000.1-HelloWorld/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _000._1_HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
